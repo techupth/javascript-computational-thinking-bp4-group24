d31d5c3a2f32ae7fbc707724234ba52e92307953
zvryn <sarinkanok@gmail.com>
2024-05-29 10:04:08 +0700

    feat: add completed assignment computational thinking

--- ex-5.js
+++ ex-5.js
@@ -1,84 +1,79 @@
-let orders = [
+let testScore =[
   {
-    id: 1,
-    customerName: "Brenn Hugk",
-    productName: "Chocolate - Milk, Callets",
-    productPrice: 94690,
-    productQuantity: 35,
-    creditCardType: "solo",
+      name:"James",
+      score:40,
   },
   {
-    id: 2,
-    customerName: "Celia Dary",
-    productName: "Bread - Pumpernickle, Rounds",
-    productPrice: 10746,
-    productQuantity: 87,
-    creditCardType: "jcb",
+      name:"Ann",
+      score:80,
   },
   {
-    id: 3,
-    customerName: "Toinette Blindermann",
-    productName: "Bar - Granola Trail Mix Fruit Nut",
-    productPrice: 94191,
-    productQuantity: 31,
-    creditCardType: "switch",
+      name:"Joe",
+      score:52,
   },
   {
-    id: 4,
-    customerName: "Anjela Joannet",
-    productName: "Cheese - Parmesan Cubes",
-    productPrice: 5131,
-    productQuantity: 96,
-    creditCardType: "diners-club-carte-blanche",
+      name:"Ball",
+      score:67,
   },
   {
-    id: 5,
-    customerName: "Kennith Bussons",
-    productName: "Wine - White, Pinot Grigio",
-    productPrice: 94432,
-    productQuantity: 75,
-    creditCardType: "jcb",
+      name:"Mick",
+      score:89,
   },
   {
-    id: 6,
-    customerName: "Birdie Shepland",
-    productName: "Tomato Paste",
-    productPrice: 5845,
-    productQuantity: 65,
-    creditCardType: "visa-electron",
-  },
-  {
-    id: 7,
-    customerName: "Willyt Helks",
-    productName: "Asparagus - Green, Fresh",
-    productPrice: 89253,
-    productQuantity: 26,
-    creditCardType: "laser",
-  },
-  {
-    id: 8,
-    customerName: "Bill Champion",
-    productName: "Wine - Red, Harrow Estates, Cab",
-    productPrice: 922100,
-    productQuantity: 40,
-    creditCardType: "china-unionpay",
-  },
-  {
-    id: 9,
-    customerName: "Cris Druce",
-    productName: "Bar Mix - Lemon",
-    productPrice: 83104,
-    productQuantity: 6,
-    creditCardType: "jcb",
-  },
-  {
-    id: 10,
-    customerName: "Darnell O'Currine",
-    productName: "Lime Cordial - Roses",
-    productPrice: 99041,
-    productQuantity: 75,
-    creditCardType: "visa-electron",
-  },
+      name:"Ole",
+      score:10,
+  }
 ];
 
-// Start coding here
+
+function averageScore (testData){
+  let sum=0
+  let count=0;
+  for (i=0;i<testData.length;i++){
+      if("score" in testData[i]){
+      sum=sum+testData[i].score;
+      count++;
+      }
+  }
+  if(count===0){
+      return "No Valid Data"
+  }
+  return  sum/count;
+}
+
+console.log(averageScore(testScore));
+
+
+function findMaxMin(testData){
+  let max =testData[0].score;
+  let count=0;
+  let min =testData[0].score;
+  let maxName;
+  let minName;
+  let result;
+  // find Max Value//
+  for (let i=0;i<testData.length;i++){
+      if("score"in testData[i] && "name" in testData[i]){
+          if(testData[i].score>max){
+              max=testData[i].score;
+              maxName = testData[i].name;
+          }
+
+          if(testData[i].score<min){
+              min=testData[i].score;
+              minName = testData[i].name;
+          }
+          count++;
+      }
+      
+  }
+  if (count ===0){
+      return "No Valid Data"
+  }
+
+  return result=
+  `${maxName} has the highest score, which is ${max} points. \n
+  ${minName} has the loweset score, which is ${min} points`;
+};
+
+console.log(findMaxMin(testScore));
